bed0893ca1aa11fd233519a2cad2e5e89161b726
michele <michele.avella.98@gmail.com>
2021-04-12 16:52:48 +0200

    sq_data

--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 np.random.seed(1234)
-x=70*(np.sin(np.linspace(0,20,100)+0.3*(np.random.randint(-1,1,100))))
-np.savetxt('DATA/in_data.txt',x)
+#x=70*(np.sin(np.linspace(0,20,100)+0.3*(np.random.randint(-1,1,100))))
+#np.savetxt('DATA/in_data.txt',x)
+def f(x):
+    if x >= 0 : return 1;
+    else:       return -1;
+f=np.vectorize(f)
+x=np.sin(np.linspace(0,20,100))
+x=50*f(x)
+#plt.plot(np.linspace(0,20,100),x)
+#plt.show()
 
-
+np.savetxt('DATA/in_quadra.txt',x)
--- plot.py
+++ plot.py
@@ -1,9 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 x=np.loadtxt('DATA/in_data.txt')
-y=np.loadtxt('DATA/out_data.txt')
-plt.plot(x)
-plt.plot(y)
-plt.show()
-#plt.savefig('plot.png')
+y=np.loadtxt('DATA/out_data_fixed.txt')
+n=np.arange(len(x))
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=n, y=x,
+                    mode='lines',
+                    name='in signal'))
+
+fig.add_trace(go.Scatter(x=n, y=y,
+                    mode='lines',
+                    name='out signal'))
+
+
+fig.write_image("plot.jpeg")
+fig.show()
